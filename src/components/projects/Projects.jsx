d4da797b3ef45c3824d97fa60e04c72770d4e3cb
Burak Sürümcüoğlu <bsurumcuoglu@gmail.com>
2023-10-30 02:18:21 +0100

    first commit

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,33 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { projectsData } from "./Data";
-import { projectsNav } from './Data';
+import { projectsNav } from "./Data";
 import ProjectItems from "./ProjectItems";
 
 const Projects = () => {
-    const [item, setItem] =  useState({ name: "All" });
-    const [projects, setProjects] = useState([]);
-    const [active, setActive] = useState(0);
+  const [item, setItem] = useState({ name: "All" });
+  const [projects, setProjects] = useState([]);
+  //   const [active, setActive] = useState(0);
 
-    useEffect(() => {
-         if(item.name === "All") {
-            setProjects(projectsData);
-         }
-         else{
-            const newProjects = projectsData.filter((project) => {
-                return project.category === item.name;
-            });
-            setProjects(newProjects);
-         }
-    },[item]);
-    
-    const handleClick  = (e, index) => {
-        setItem({ name: e.target.textContent });
-        setActive(index);
-    };
+  useEffect(() => {
+    if (item.name === "All") {
+      setProjects(projectsData);
+    } else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
 
-    return (
-        <div>
-            <div className="project__filters">
+  // const handleClick  = (e, index) => {
+  //     setItem({ name: e.target.textContent });
+  //     setActive(index);
+  // };
+
+  return (
+    <div>
+      {/* <div className="project__filters">
                 {projectsNav.map((item,index) => {
                     return (
                         <span onClick={(e) => {
@@ -37,14 +36,14 @@ const Projects = () => {
                         key={index}>{item.name}</span>
                     )
                 })}
-            </div>
-            <div className="project__container container grid">
-                {projects.map((item) => {
-                    return <ProjectItems item={item} key={item.id}/>
-                })}
-            </div>
-        </div>
-    );
-}
+            </div> */}
+      <div className="project__container container grid">
+        {projects.map((item) => {
+          return <ProjectItems item={item} key={item.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,145 +1,181 @@
 import React, { useState } from "react";
 import "./qualification.css";
-import { HiOutlineAcademicCap, HiOutlineBriefcase, HiOutlineCalendar } from "react-icons/hi";
+import {
+  HiOutlineAcademicCap,
+  HiOutlineBriefcase,
+  HiOutlineCalendar,
+} from "react-icons/hi";
 
 const Qualification = () => {
-    const [toggleState, setToggleState] = useState(1)
+  const [toggleState, setToggleState] = useState(2);
 
-    const toggleTab = (index) => {
-        setToggleState(index);
-    };
-  
-    return (
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
     <section className="qualification section">
-        <h2 className="section__title">Qualification</h2>
-        <span className="section__subtitle">My Journey</span>
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My Journey</span>
 
-        <div className="qualification__container container">
-            <div className="qualification__tabs">
-                <div className={toggleState === 1 ? "qualification__button button--flex qualification__active" 
-                    : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
-                    <HiOutlineAcademicCap className="qualification__icon" />
-                    Education
-                </div>
-                <div className={toggleState === 2 ? "qualification__button button--flex qualification__active" 
-                    : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
-                    <HiOutlineBriefcase className="qualification__icon" />
-                    Experience
-                </div>
-            </div>
-
-            <div className="qualification__sections">
-                <div className={toggleState === 1 ? "qualification__content qualification__content-active"
-                    : "qualification__content"}>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Design</h3>
-                            <span className="qualification__subtitle">The Art Institute</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2016-2018
-                            </div>
-                        </div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div></div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Web Development</h3>
-                            <span className="qualification__subtitle">Full Sail University</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2018-2021
-                            </div>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">UI/UX Design</h3>
-                            <span className="qualification__subtitle">UX Academy</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2021-2022
-                            </div>
-                        </div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className={toggleState === 2 ? "qualification__content qualification__content-active"
-                    : "qualification__content"}>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Tech Support</h3>
-                            <span className="qualification__subtitle">PC Outlet</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2012-2015
-                            </div>
-                        </div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div></div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Junior Developer</h3>
-                            <span className="qualification__subtitle">Discord</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2015-2016
-                            </div>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">UX Designer</h3>
-                            <span className="qualification__subtitle">Apple Inc</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2016-2022
-                            </div>
-                        </div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
-                    <div className="qualification__data">
-                        <div></div>
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
-                        </div>
-                        <div>
-                            <h3 className="qualification__title">Senior Developer</h3>
-                            <span className="qualification__subtitle">Freelance - Remote</span>
-                            <div className="qualification__calendar">
-                                <HiOutlineCalendar className="qualification__calendar-icon" />
-                                2019-Present
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button button--flex qualification__active"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <HiOutlineAcademicCap className="qualification__icon" />
+            Education
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button button--flex qualification__active"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <HiOutlineBriefcase className="qualification__icon" />
+            Experience
+          </div>
         </div>
-    </section> 
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Management Msc.</h3>
+                <span className="qualification__subtitle">
+                  Czestochowa University of Technology
+                </span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  2023-2025
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Computer Engineering</h3>
+                <span className="qualification__subtitle">
+                  Manisa Celal Bayar University
+                </span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  2018-2022
+                </div>
+              </div>
+            </div>
+            {/* <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title"></h3>
+                <span className="qualification__subtitle">UX Academy</span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  2021-2022
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div> */}
+          </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Full Stack Developer</h3>
+                <span className="qualification__subtitle">C2B Technology</span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  01.2023-Present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Full Stack Developer Tutor
+                </h3>
+                <span className="qualification__subtitle">Develhope</span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  03.2023-10.2023
+                </div>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Frontend Developer</h3>
+                <span className="qualification__subtitle">Simetri8</span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  12.2022-01.2023
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">
+                  Full Stack Developer Intern
+                </h3>
+                <span className="qualification__subtitle">
+                  Hamdi Küçük A.Ş.
+                </span>
+                <div className="qualification__calendar">
+                  <HiOutlineCalendar className="qualification__calendar-icon" />
+                  03.2022-06.2022
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
 
 export default Qualification;
