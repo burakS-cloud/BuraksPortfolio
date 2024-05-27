4334a7fac6c503d0af4a0ee0cb7c250118ba1a98
Burak Sürümcüoğlu <bsurumcuoglu@gmail.com>
2024-05-27 13:52:20 +0200

    fix alignment of experience

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -108,6 +108,11 @@ const Qualification = () => {
             }
           >
             <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
               <div>
                 <h3 className="qualification__title">Full Stack Developer</h3>
                 <span className="qualification__subtitle">Grand Parade</span>
@@ -115,10 +120,6 @@ const Qualification = () => {
                   <HiOutlineCalendar className="qualification__calendar-icon" />
                   01.2024-Present
                 </div>
-              </div>
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
               </div>
             </div>
             <div className="qualification__data">
